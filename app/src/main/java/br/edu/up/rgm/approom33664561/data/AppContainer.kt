ffe938c6c705f38a57b9eadd336a3fd638db3f6c
FelipePupo2122 <felipepupo53@gmail.com>
2024-11-25 19:00:39 -0300

    Ajustes finais

--- app/src/main/java/br/edu/up/rgm/approom33664561/data/AppContainer.kt
+++ app/src/main/java/br/edu/up/rgm/approom33664561/data/AppContainer.kt
@@ -9,13 +9,8 @@ interface AppContainer {
     val itemsRepository: ItemsRepository
 }
 
-/**
- * [AppContainer] implementation that provides instance of [OfflineItemsRepository]
- */
 class AppDataContainer(private val context: Context) : AppContainer {
-    /**
-     * Implementation for [ItemsRepository]
-     */
+
     override val itemsRepository: ItemsRepository by lazy {
         OfflineItemsRepository(InventoryDatabase.getDatabase(context).itemDao())
     }
